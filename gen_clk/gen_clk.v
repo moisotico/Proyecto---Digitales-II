c6ff03b3c73677fd053f36bc1b67aab60d8a8d8a
moisotico <moiso.camposcr@gmail.com>
2019-05-15 01:02:59 -0600

    gen_clk.v completo, probador pendiente

--- gen_clk/gen_clk.v
+++ gen_clk/gen_clk.v
@@ -1,11 +1,33 @@
 `timescale 1ns/1ps
 
 module gen_clk(
-	clk_8f, rst, enb
+	clk_8f, rst, enb,
 	clk_2f, clk_f	
 );
 
+    input clk_8f, rst, enb;
+    output reg clk_2f, clk_f;   
 
+    reg [0:1] counter;
+
+    always@(posedge clk_8f) begin
+        
+        if(rst) begin
+            clk_2f <=   1'b0;
+            clk_f <=   1'b0;
+        end
+
+        else begin
+            if(enb) begin
+                if (counter < 3) counter <= counter + 'b1;  // counter de clk_2f y clk_f
+                else begin
+                    counter <= 'b0;                         //resetear counter
+                    if(~clk_2f) clk_2f <= ~clk_2f;          //clk 
+                    if(~clk_2f & ~clk_f) clk_f <= ~clk_f; 
+                end
+            end
+        end
+    end
 
 
 endmodule
