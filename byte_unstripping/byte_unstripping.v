d60ecd82fe7cd62a070992b192cf7f094f058b11
moisotico <moiso.camposcr@gmail.com>
2019-05-29 00:35:34 -0600

    cambios adicionales al byte unstripping

--- byte_unstripping/byte_unstripping.v
+++ byte_unstripping/byte_unstripping.v
@@ -5,12 +5,12 @@
 	*@date   21/05/2019
 	*@brief  Descripcion conductual del byte unstripping    
 **/
-`timescale 1ns/1ps
+
+//`timescale 1ns/1ps
 
 module byte_unstripping(
                         output reg[7:0] data_demux,     //salida a data demux, cambia con clk_2f
                         output reg      valid_demux,
-
                         input           clk_f,
                         input           clk_2f,
                         input           reset_L,
@@ -27,15 +27,15 @@ module byte_unstripping(
     reg             selector; 
     
 //bloques always
+       /**
     always@(*)begin
         //activa la entrada
-       /**
         if (reset_L==0) begin
             selector = 'b0;
         end 
-        **/
     end
     
+        **/
     /**
     always@(posedge clk_f)begin
     end
@@ -45,46 +45,56 @@ module byte_unstripping(
     always@(posedge clk_2f)begin
         if (!reset_L)begin
             selector <= 'b0;
-            data_validated_0 <= 'b0;
-            data_validated_1 <= 'b0;
+            data_validated_0    <= 'b0;
+            data_validated_1    <= 'b0;
+            valid_demux         <= 'b0;              
         end
         
         // agregar entradas
         // dar prioridad a data_strip_0
-        else begin
+
+        else begin 
             case (selector)
+                //default
                 0:
                     if (valid_stripe_0) begin
-                        data_validated_0 <= data_stripe_0;
+                        data_validated_0 <= data_stripe_0; 
                         selector <= ~selector; 
+                        valid_demux <= valid_stripe_0;              //valid_demux selection
                     end
                     else begin
                         data_validated_0 <= data_validated_0;
+                        valid_demux <= 'b0;
                     end
-
+                    
                 1:
                     if (valid_stripe_1) begin
                         data_validated_1 <= data_stripe_1;
                         selector <= ~selector; 
+                        valid_demux <= valid_stripe_1;              //valid_demux selection
                     end
                     else begin
-                         data_validated_1 <= data_validated_1;
+                        data_validated_1 <= data_validated_1;
+                        valid_demux <= 'b0;
                     end
             endcase
-            //mover a posedge2f
             
-            //  salida data_demux
          end
  
-        // validar entradas
+        // validar salida data_demux
         if (valid_demux) begin
-            if (selector)begin
-                data_demux <= data_validated_1; 
-            end
-            
-            else begin
+            if (selector==0)begin
                 data_demux <= data_validated_0; 
             end
+                
+            else begin
+                data_demux <= data_validated_1; 
+            end
+        end
+            //Si es invalido, selector == 0
+        else begin
+                selector <= 'b0;
+                data_demux <= 'b0;
         end
     end
 
