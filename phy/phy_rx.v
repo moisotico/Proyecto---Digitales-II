c12bac4d492d5823c1cc8b3addfc89de9b4151a7
giancamarin <gianca.1996mh@gmail.com>
2019-05-29 20:57:26 -0600

    Renombrar entradas y salidas segun señalizacion

--- phy/phy_rx.v
+++ phy/phy_rx.v
@@ -1,13 +1,14 @@
 module phy_rx(
     input clk_8f,
+    input clk_2f,
+    input clk_f,
     input reset_L,
-    input in_0,
-    input in_1,
-	input enable,
-    output reg [7:0] data_out_0_cond,
-    output reg [7:0] data_out_1_cond,
-	output reg valid_out_0_cond,
-	output reg valid_out_1_cond
+    input transfer_0,
+    input transfer_1,
+    output reg [7:0] data_out_0,
+    output reg [7:0] data_out_1,
+	output reg valid_out_0,
+	output reg valid_out_1
 );
 /*AUTOWIRE*/
 // Beginning of automatic wires (for undeclared instantiated-module outputs)
@@ -21,31 +22,20 @@ wire			valid_par_0;		// From line0 of serialtopar.v, ...
 wire [7:0]		data_par_1;		// From line0 of serialtopar.v, ...
 wire			valid_par_1;		// From line0 of serialtopar.v, ...
 wire			valid_unstripped;	// From byte_uns of byte_unstripping.v
-// End of automatics
-wire clk_2f;
-wire clk_f;
 
 always @(posedge clk_2f) begin
     if(~reset_L) begin
-        data_out_0_cond<='b0;
-        data_out_1_cond<='b0;
-		valid_out_0_cond<='b0;
-		valid_out_1_cond<='b0;
+        data_out_0<='b0;
+        data_out_1<='b0;
+		valid_out_0<='b0;
+		valid_out_1<='b0;
     end else begin
-        data_out_0_cond<=data_demux_0;
-        data_out_1_cond<=data_demux_1;
-		valid_out_0_cond<=valid_demux_0;
-		valid_out_1_cond<=valid_demux_1;
+        data_out_0<=data_demux_0;
+        data_out_1<=data_demux_1;
+		valid_out_0<=valid_demux_0;
+		valid_out_1<=valid_demux_1;
     end
 end
-
-gen_clk clocks (/*autoinst*/
-		// Outputs
-		.clk_2f			(clk_2f),
-		.clk_f			(clk_f),
-		// Inputs
-		.clk_8f			(clk_8f),
-		.enable			(enable));
 
 serialtopar line0(/*AUTOINST*/
 		  // Outputs
@@ -55,7 +45,7 @@ serialtopar line0(/*AUTOINST*/
 		  .clk_f		(clk_f),
 		  .clk_8f		(clk_8f),
 		  .reset_L		(reset_L),
-		  .in			(in_0));
+		  .in			(transfer_0));
 serialtopar line1(/*AUTOINST*/
 		  // Outputs
 		  .data_par		(data_par_1[7:0]),
@@ -64,7 +54,7 @@ serialtopar line1(/*AUTOINST*/
 		  .clk_f		(clk_f),
 		  .clk_8f		(clk_8f),
 		  .reset_L		(reset_L),
-		  .in			(in_1));
+		  .in			(transfer_1));
 byte_unstripping byte_uns(/*AUTOINST*/
 			  // Outputs
 			  .data_unstripped	(data_unstripped[7:0]),
@@ -87,7 +77,4 @@ demux demux0(/*AUTOINST*/
 	     .clk_2f			(clk_2f),
 	     .reset_L			(reset_L),
 	     .data_unstripped		(data_unstripped[7:0]));
-
-
-
 endmodule
