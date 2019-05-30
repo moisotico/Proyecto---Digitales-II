2d458b924afb4306b4038c74d687886529f6367f
Esteban Valverde Hernández <e.valverde95@hotmail.com>
2019-05-29 18:54:23 -0600

    Datos inválidos entre sucesivos envíos válidos en probador

--- phy_rx/probador.v
+++ phy_rx/probador.v
@@ -184,6 +184,32 @@ module probador(
         @(posedge clk_8f);
         in_0 <= 1;
         in_1 <= 1;
+
+        //Dato invalido
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
         // Se envia por lane0 88 y lane 1 22
         @(posedge clk_8f);
         in_0 <= 1;
@@ -234,6 +260,31 @@ module probador(
         @(posedge clk_8f);
         in_0 <= 1;
         in_1 <= 1;
+        //Dato invalido
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 1;
+        in_1 <= 1;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
+        @(posedge clk_8f);
+        in_0 <= 0;
+        in_1 <= 0;
         // Se envia por lane0 66 y lane 1 44
         @(posedge clk_8f);
         in_0 <= 0;
